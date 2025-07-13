d3ddaf2e752af976a14560f06e48e0f78c0a82ee
Adrien-Ray <a.raymond@codeur.online>
2025-07-13 16:39:44 +0200

    multi piste selected on loop - with compatibility random mode #5

--- assets/script.js
+++ assets/script.js
@@ -6,6 +6,7 @@ function clickItem(path, id) {
         element.classList.toggle('current');
     })
     document.getElementById(id).classList.toggle('current');
+    return;
 }
 
 function constructList(listFiles, folderMusic) {
@@ -14,34 +15,110 @@ function constructList(listFiles, folderMusic) {
         const file = listFiles[i].name;
         const dir = listFiles[i].path;
         const duration = listFiles[i].duration;
-        let liDom = `<li><span id="element_${i}" data-object='{"file":"${file}","dir":"${dir}","duration":"${duration}"}' onclick="clickItem('${dir}/${file}', 'element_${i}');">${dir}/<b>${file}</b></span></li>`;
+        
+        let liDom = `<li><input class="multiPistModeCheckbox" type="checkbox" onclick="document.getElementById('element_${i}').classList.toggle('multiPistModeCheck');"><span id="element_${i}" data-object='{"file":"${file}","dir":"${dir}","duration":"${duration}"}' onclick="clickItem('${dir}/${file}', 'element_${i}');">${dir}/<b>${file}</b></span></li>`;
         contentUl = contentUl+liDom
     }    
     document.getElementById('listFiles').innerHTML = contentUl;
+    return;
 }
 
 function randomMode() {
     if (! document.getElementById('randomMode').classList.contains('randomModeActive')) {
+        document.getElementById('lecteur').loop = false;
         function randomCall() {
-            const itemDOM = document.querySelectorAll('ul#listFiles > li > span');
+            console.log('exec func randomCall');
+            document.getElementById('lecteur').loop = false;
+            let itemDOM;
+            if (document.querySelector('button#multiPistMode.multiPistModeActive')) {
+                itemDOM = document.querySelectorAll('ul#listFiles > li > span.multiPistModeCheck');
+            } else {
+                itemDOM = document.querySelectorAll('ul#listFiles > li > span');
+            }
             // console.log(itemDOM);
             let randomIndex = Math.floor(Math.random() * itemDOM.length);
             let randomItem = itemDOM[randomIndex];
             // console.log(randomItem);
             randomItem.click();
-            document.getElementById('lecteur').addEventListener('ended', () => {
-                randomCall();
-                return;
-            });
+            function listenEnded() {
+                    randomCall();
+                    return;
+            }
+            document.getElementById('lecteur').addEventListener('ended',listenEnded, { once: true });
         };
         randomCall();
         document.getElementById('randomMode').addEventListener('click', () => {
-            // clearInterval(randomCall);
             randomCall = null;
         });
+    } else {
+        document.getElementById('lecteur').loop = true;
     }
     document.getElementById('randomMode').classList.toggle('randomModeActive');
-    document.getElementById('lecteur').loop = !document.getElementById('lecteur').loop;
+    // document.getElementById('lecteur').loop = !document.getElementById('lecteur').loop;
+    document.getElementById('randomMode').addEventListener('click', () => {
+        return;
+    })
+}
+
+function multiPistMode() {
+
+    function nextCall() {
+            console.log('exec func nextCall');
+            
+            document.getElementById('lecteur').loop = false;
+            let itemDOM;
+            if (document.querySelector('button#multiPistMode.multiPistModeActive')) {
+                itemDOM = document.querySelectorAll('ul#listFiles > li > span.multiPistModeCheck');
+            } else {
+                console.error('La fonction nextCall ne devrai pas être appelé si multiPistMode is disabled');
+                return;
+            }
+            let arrayLength = itemDOM.length;
+            let itemDOMArray = Array.from(itemDOM);
+            let actualIndex = itemDOMArray.findIndex(item => item.classList.contains('current'));
+            let nextIndex = actualIndex+1;
+            if(nextIndex >= arrayLength) {
+                nextIndex = 0;
+            };
+            let nextItem = itemDOM[nextIndex];
+            setTimeout(() => {
+                nextItem.click();
+            }, 100);
+            function listenEnded() {
+                    nextCall();
+                    // document.getElementById('lecteur').removeEventListener('ended', listenEnded);
+                    return;
+            }
+            document.getElementById('lecteur').addEventListener('ended',listenEnded, { once: true });
+        };
+
+
+
+
+    document.getElementById('multiPistMode').classList.toggle('multiPistModeActive');
+    document.getElementById('listFiles').classList.toggle('multiPistModeEnabled');
+    if (document.getElementById('multiPistMode').classList.contains('multiPistModeActive')) { // not already active
+        document.getElementById('lecteur').loop = false;
+        // console.log('now enabled');
+        // check si randomMode + écoute si toggle class random active pour call ou pas une fonction de lecture à la suite des items multiPistModeCheck
+        if (!document.getElementById('randomMode').classList.contains('randomModeActive')) {
+            // si pas random, call nextCall
+            nextCall();
+        } else {
+            // sinon écoute le désabonnement à randomMode
+            document.getElementById('randomMode').addEventListener('click',() => {
+                nextCall();
+            })
+        }
+        // si click sur modeMulti, exit return
+        document.getElementById('multiPistMode').addEventListener('click', () => {
+            return;
+        })
+    } else { // already multi
+        document.getElementById('lecteur').loop = true;
+        // console.log('now disabled');
+        return;
+    }
 }
 
 fetch('./assets/data.json')
